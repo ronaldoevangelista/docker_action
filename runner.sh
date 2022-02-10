49b712bb0015999237d80b0e783f5f9ac309eefe
Ronaldo Evangelista <ronaldinho.evangelista@gmail.com>
2022-02-10 16:29:40 -0300

    add distro name

--- runner.sh
+++ runner.sh
@@ -8,9 +8,12 @@ export PROJECT_NAME=${PROJECT_NAME:-"overview"}
 printf "%s\n" "$DISTRO_NAME"  | sed "$option_sed_indent"
 printf "%s\n" "$PROJECT_NAME" | sed "$option_sed_indent"
 
+IMAGE_NAME=${PROJECT_NAME}:devel
+
 echo "::group::Build Image"
-docker build --rm --build-arg PROJECT_NAME="$PROJECT_NAME" -t "$DISTRO_NAME" .
+docker build  --rm \
+    --build-arg PROJECT_NAME=$DISTRO_NAME \
+    -t ${IMAGE_NAME} .
 echo "::endgroup::"
 
-docker run -t --rm \
-  "${PROJECT_NAME}"
+
