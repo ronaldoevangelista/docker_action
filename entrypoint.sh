416ac3d43446002915aec2c2daa6dba12e54656a
Ronaldo Evangelista <ronaldinho.evangelista@gmail.com>
2022-02-11 00:37:49 -0300

    update minor changes

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,17 @@
 #!/bin/sh -l
 
+export DISTRO="${DISTRO:-}"
+
 echo "Hello $1"
+
 time=$(date)
 echo ::set-output name=time::$time
 
 version=$(cat /etc/issue)
 
 echo "version ${version}"
-
 echo "::set-output name=version::${version}"
+
+echo ":: entrypoint :: DISTRO ${DISTRO}"
+
+exec "$@"
--- runner.sh
+++ runner.sh
@@ -1,19 +1,75 @@
 #!/bin/bash
 
-option_sed_indent='s/^/      /'
+OPT_SED_INDENT='s/^/      /'
 
-export DISTRO_NAME=${DISTRO_NAME:-"nvidia/opengl:1.0-glvnd-runtime-ubuntu16.04"}
-export PROJECT_NAME=${PROJECT_NAME:-"overview"}
+# Settings
+export PROJECT="${PROJECT:-"overview"}"
+export DISTRO="${DISTRO:-"bionic"}"
 
-printf "%s\n" "$DISTRO_NAME"  | sed "$option_sed_indent"
-printf "%s\n" "$PROJECT_NAME" | sed "$option_sed_indent"
+PROJECT_PATH="$HOME/workspace/${PROJECT}_${DISTRO}"
+BASE_IMAGE="nvidia/opengl:1.0-glvnd-runtime-ubuntu18.04";
 
-IMAGE_NAME=${PROJECT_NAME}:devel
+USER="$(id -nu)"
+UUID="$(id -u)"
+UGID="$(id -g)"
 
-echo "::group::Build Image"
-docker build  --rm \
-    --build-arg PROJECT_NAME=$DISTRO_NAME \
-    -t ${IMAGE_NAME} .
-echo "::endgroup::"
+export PROJECT
+export PROJECT_PATH
+export USER
+export UUID
+export UGID
+
+CONTAINER_USER=${USER}-${PROJECT}
+
+export IMAGE_NAME=${PROJECT}-${DISTRO}:devel
+
+printf "%s\n" "$PROJECT"      | sed "$OPT_SED_INDENT"
+printf "%s\n" "$DISTRO"       | sed "$OPT_SED_INDENT"
+printf "%s\n" "$PROJECT_PATH" | sed "$OPT_SED_INDENT"
+printf "%s\n" "$BASE_IMAGE"   | sed "$OPT_SED_INDENT"
+printf "%s\n" "$IMAGE_NAME"   | sed "$OPT_SED_INDENT"
+
+docker build --compress \
+             --no-cache=true \
+             --build-arg BASE_IMAGE=${BASE_IMAGE} \
+             -t ${IMAGE_NAME} .
+
+docker run -t -i --rm \
+            --net host \
+            --name overview \
+            -e DISTRO=${DISTRO} 
+            ${IMAGE_NAME}
 
 
+# docker run -d -t -i --rm -p 5051:5051
+# --network=training_net -e NODE_ENV=${NODE_ENV} -e DATABASE_HOST=${DATABASE_HOST}
+#  -e DATABASE_USERNAME=${DATABASE_USERNAME} -e DATABASE_PASSWORD=${DATABASE_PASSWORD} 
+#  -e DATABASE=${DATABASE} -e SERVICE_TOKEN=${SERVICE_TOKEN} 
+#  -e HOST_TRAINING_COURSE=${HOST_TRAINING_COURSE} -e SECRET=${SECRET} 
+#  -e SECRET_KEY_PASSWORD=${SECRET_KEY_PASSWORD} --name training-user training-user:latest
+
+# DISTRO=${DISTRO:-"bionic"}
+# BASE_IMAGE="nvidia/opengl:1.0-glvnd-runtime-ubuntu16.04";
+
+
+# export DISTRO_NAME=${DISTRO_NAME:-"nvidia/opengl:1.0-glvnd-runtime-ubuntu16.04"}
+# export PROJECT_NAME=${PROJECT_NAME:-"overview"}
+
+# printf "%s\n" "$BASE_IMAGE"  | sed "$option_sed_indent"
+
+
+# IMAGE_NAME=${PROJECT_NAME}:devel
+
+# printf "%s\n" "$IMAGE_NAME" | sed "$option_sed_indent"
+
+# CONTAINER_USER=${USER}-${PROJECT}
+
+# docker build  \
+#     --build-arg BASE_IMAGE=$BASE_IMAGE \
+#     --build-arg USER=$CONTAINER_USER \
+#     --compress --no-cache=true \
+#     -t ${IMAGE_NAME} .
+
+# docker run -d -t -i --rm \
+#     --net host \
+#     --name overview} overview:devel
