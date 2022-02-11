92a6e35144fcb275ed165c5367ae72a4cf303808
Ronaldo Evangelista <ronaldinho.evangelista@gmail.com>
2022-02-11 20:47:47 -0300

    add new parameter update

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,8 @@
 #!/bin/sh -l
 
-export PROJECT=${PROJECT:-}
+export PROJECT="${PROJECT:-}"
+export BOOTSTRAP="${BOOTSTRAP:-}"
+
 export AUTOPROJ_INSTALL_URL=https://raw.githubusercontent.com/rock-core/autoproj/master/bin/autoproj_install
 
 BOOTSTRAP_URL=git@github.com:romulogcerqueira/sonar_simulation-buildconf.git
--- runner.sh
+++ runner.sh
@@ -1,32 +1,26 @@
 #!/bin/bash
 
-OPT_SED_INDENT='s/^/      /'
+OPTION_SED_INDENT='s/^/      /'
 
 # Settings
 export PROJECT=${PROJECT:-"sonarsim"}
 
-if [ -f "$DISTRO" ]; then export DISTRO=$(<"$DISTRO"); fi
-
-OPTION_SED_INDENT='s/^/      /'
-
+if [ -f "$DISTRO" ]; then export DISTRO=$(<${DISTRO:-"focal"}); fi
+if [ -f "$BOOTSTRAP" ]; then export BOOTSTRAP=$(<${BOOTSTRAP:-}); fi
 
 export IMAGE_NAME=${PROJECT}-${DISTRO}:devel
 export USER="$(id -nu)"
 export UUID="$(id -u)"
 export UGID="$(id -g)"
 export WORKSPACE="~/workspace/${PROJECT}_${DISTRO}"
-export BOOTSTRAP=${BOOTSTRAP}
-
-printf "%s\n" "$DISTRO" | sed "$OPTION_SED_INDENT"
-
 
 case ${DISTRO} in
     "xenial")
-        IMAGE_BASE="nvidia/opengl:1.0-glvnd-runtime-ubuntu16.04";;
+        export IMAGE_BASE="nvidia/opengl:1.0-glvnd-runtime-ubuntu16.04";;
     "bionic")
-        IMAGE_BASE="nvidia/opengl:1.0-glvnd-runtime-ubuntu18.04";;
+        export IMAGE_BASE="nvidia/opengl:1.0-glvnd-runtime-ubuntu18.04";;
     "focal")
-        IMAGE_BASE="nvidia/opengl:1.0-glvnd-runtime-ubuntu20.04";;
+        export IMAGE_BASE="nvidia/opengl:1.0-glvnd-runtime-ubuntu20.04";;
     *)
         echo "Ubuntu distro invalid! Checks the file settings.sh"
         exit 1;;
