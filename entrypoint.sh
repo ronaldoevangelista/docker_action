75d069ed94f18e5de85cd64b3c12f6d6e3505bf7
Ronaldo Evangelista <ronaldinho.evangelista@gmail.com>
2022-02-11 20:11:49 -0300

    fix minor changes

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,10 +4,6 @@ export PROJECT="${PROJECT:-}"
 
 echo "ENTRYPOINT::PROJECT ${PROJECT}"
 
-time=$(date)
-echo ::set-output name=time::$time
-
 version=$(cat /etc/issue)
 
 echo "version ${version}"
-echo "::set-output name=version::${version}"
--- runner.sh
+++ runner.sh
@@ -5,29 +5,28 @@ OPT_SED_INDENT='s/^/      /'
 # Settings
 export PROJECT=${PROJECT:-"overview"}
 export DISTRO=${DISTRO:-"bionic"}
-export BASE_IMAGE=${BASE_IMAGE:-"nvidia/opengl:1.0-glvnd-runtime-ubuntu18.04"};
 export IMAGE_NAME=${PROJECT}-${DISTRO}:devel
 
-echo "::group::List Parameter"
-printf "%s\n" "$PROJECT"      | sed "$OPT_SED_INDENT"
-printf "%s\n" "$DISTRO"       | sed "$OPT_SED_INDENT"
-printf "%s\n" "$BASE_IMAGE"   | sed "$OPT_SED_INDENT"
-printf "%s\n" "$IMAGE_NAME"   | sed "$OPT_SED_INDENT"
-echo "::endgroup::"
+case ${DISTRO} in
+    "xenial")
+        BASE_IMAGE="nvidia/opengl:1.0-glvnd-runtime-ubuntu16.04";;
+    "bionic")
+        BASE_IMAGE="nvidia/opengl:1.0-glvnd-runtime-ubuntu18.04";;
+    "focal")
+        BASE_IMAGE="nvidia/opengl:1.0-glvnd-runtime-ubuntu20.04";;
+    *)
+        echo "Ubuntu distro invalid! Checks the file settings.sh"
+        exit 1;;
+esac
 
-echo "::group::Build Image"
 docker build --rm \
              --build-arg BASE_IMAGE=${BASE_IMAGE} \
              --compress \
              --no-cache=true \
              -t ${IMAGE_NAME} .
-echo "::endgroup::"
 
-echo "::group::Run docker"
 docker run -t --rm \
             --net host \
             --name overview \
             -e PROJECT=${PROJECT} \
             ${IMAGE_NAME}
-echo "::endgroup::"
-
